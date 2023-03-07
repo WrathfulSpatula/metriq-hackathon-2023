fb18af21a9c3a4d7bcfe7ba8358aa4ad1decbcf4
Daniel Strano <stranoj@gmail.com>
2023-03-07 13:50:53 -0500

    'before or during'

--- src/views/Home.js
+++ src/views/Home.js
@@ -35,7 +35,7 @@ const Home = () => {
       </FieldWideRow>
       <br />
       <FieldWideRow className={'text-left'}>
-        To register for the event, you must <a target='_blank' href='https://metriq.info/Register' rel='noreferrer'>create an account</a> on the Metriq web app, but no other special event registration is necessary. You can register at any time during the event, to be eligible for prizes.
+        To register for the event, you must <a target='_blank' href='https://metriq.info/Register' rel='noreferrer'>create an account</a> on the Metriq web app, but no other special event registration is necessary. You can register at any time before or during the event, to be eligible for prizes.
       </FieldWideRow>
       <br />
       <FieldWideRow className={'text-left'}>
